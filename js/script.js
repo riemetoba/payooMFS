4320796b4b362ea8868d3843cb752924c3f0872d
Toba <riemehasan06@gmail.com>
2026-01-17 19:40:22 +0600

    en

--- js/script.js
+++ js/script.js
@@ -4,25 +4,51 @@ let pinInput = document.getElementById("pinInput");
 let loginBtn = document.getElementById("loginBtn");
 let errorMsg = document.getElementById("errorMsg");
 let secondPart = document.getElementById("secondPart");
-// =====================> 
-    loginBtn.addEventListener("click",()=>{
-        if (numberInput.value == "017" && pinInput.value == 6666) {
-            // console.log("thik ache"); 
-            numberInput.value = "";
-            pinInput.value = "";
-            secondPart.removeAttribute("hidden");
-            firstPart.setAttribute("hidden", true);
-        }
-     else if (numberInput.value == "" && pinInput.value == "") {
-        // console.log("input dao"); 
-        errorMsg.removeAttribute("hidden");
-        let newErrorMsg = "Plaese type your Number & Pin.";
-        errorMsg.innerText = newErrorMsg;
-        errorMsg.style.background = "blue";
-        errorMsg.style.padding = "10px 20px";
-        errorMsg.style.borderRadius = "10px";
-     }
-        else{
-            errorMsg.removeAttribute("hidden");
-        }
-    })
+// =====================>
+loginBtn.addEventListener("click", () => {
+  if (numberInput.value == "017" && pinInput.value == 6666) {
+    // console.log("thik ache");
+    numberInput.value = "";
+    pinInput.value = "";
+    secondPart.removeAttribute("hidden");
+    firstPart.setAttribute("hidden", true);
+  } else if (numberInput.value == "" && pinInput.value == "") {
+    // console.log("input dao");
+    errorMsg.removeAttribute("hidden");
+    let newErrorMsg = "Plaese type your Number & Pin.";
+    errorMsg.innerText = newErrorMsg;
+    errorMsg.style.background = "blue";
+    errorMsg.style.padding = "10px 20px";
+    errorMsg.style.borderRadius = "10px";
+  } else {
+    errorMsg.removeAttribute("hidden");
+  }
+});
+// ===========================>
+// LogOut Button er kaj ======>
+let logOutBtn = document.getElementById("logOutBtn");
+logOutBtn.addEventListener("click",()=>{
+    firstPart.removeAttribute("hidden");
+    secondPart.setAttribute("hidden", true);
+})
+// LogOut Button er kaj ======>
+    // Add Money part=========>
+        let addMoneyScreen = document.getElementById("addMoneyScreen");
+        let addMoneyBtn = document.getElementById("addMoneyBtn");
+        let addClick = document.getElementById("addClick");
+        let balance = document.getElementById("balance");
+        let addAmount = document.getElementById("addAmount");
+        let addPin = document.getElementById("addPin");
+        addMoneyBtn.addEventListener("click",()=>{
+            if (addPin.value == 6666) {
+                addAmount.innerText = "";
+                addPin.value = "";
+            }
+        })
+        addClick.addEventListener("click",()=>{
+           let total = parseInt(balance.innerText);
+           let jogAmount = parseInt(addAmount.value);
+           balance.innerText = total+jogAmount
+           
+        })
+    // Add Money part=========>
